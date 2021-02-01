ebdcbfd29c4bc8f6c75cc51a2a5c18bb3683f79c
jayryanj <43054912+jayryanj@users.noreply.github.com>
2021-02-01 12:04:25 -1000

    Cleaned up getInterface() in scanner.js

--- server/scanner.js
+++ server/scanner.js
@@ -15,28 +15,24 @@ function getInterface() {
     // 'interface' is a reserved word in strict mode, so it's abbreviated here. 
     const intfs = os.networkInterfaces();
     const platform = os.platform();
-    let intfNames;
-    let result;
-
+    let intfNames, intfName, netData;
 
     if (platform === "linux" || platform === "win32") {
         intfNames = Object.keys(intfs);
-        const chosenIntf = intfNames.find(intf => {
+        intfName = intfNames.find(intf => {
             return (intf == "Ethernet" || intf == "Wi-Fi" || intf.substring(0,2) == "en" || intf.substring(0, 2) == "wl")
         }); 
 
-        const netData = intfs[chosenIntf].find(net => {
+        netData = intfs[intfName].find(net => {
             return net.family === "IPv4"
         })
-
-        result = {
-            name: chosenIntf,
-            address: netData.address,
-            bits: 32 - parseInt(netData.cidr.slice(-2)) // The slice will break for large subnets
-        }
     }
 
-    return result;
+    return {
+        name: intfName,
+        address: netData.address,
+        bits: 32 - parseInt(netData.cidr.slice(-2)) // The slice will break for large subnets
+    };
 } 
 
 function getAddresses() {
@@ -44,6 +40,8 @@ function getAddresses() {
     // This function is going to be some interesting math
     // Total # of addressses can be calculated by taking 2^n where n is the bits available to the subnet 
 }
+
+
 console.log(getInterface());
 
 module.exports = scan;
